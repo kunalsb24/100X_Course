2e18153b413dc8957e43bb218c573e896052493a
kunalsb24 <kunalbhajbhuje5@gmail.com>
2026-07-30 22:21:51 +0530

    Added the Schema

---
+++ Week_8-course-selling-app/db.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+mongoose.connect("mongodb+srv://kunalbhajbhuje5_db_user:plqDRO7zenpfzThp@cluster0.wbdnjio.mongodb.net/coursera-app")
+
+const userSchema = new Schema({
+  email: {type: String, unique: true},
+  password: String,
+  firstName: String,
+  lastName: String
+
+});
+
+const adminSchema = new Schema({
+  email: {type: String, unique: true},
+  password: String,
+  firstName: String,
+  lastName: String
+
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+});
+
+const purchaseSchema = Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+})
+
+const UserModel = mongoose.model('users', userSchema);
+const AdminModel = mongoose.model('admin', adminSchema);
+const CourseModel = mongoose.model('course', courseSchema);
+const PurchaseModel = mongoose.model('purchase', purchaseSchema);
+
+module.exports = {
+    UserModel,
+    AdminModel,
+    CourseModel,
+    PurchaseModel
+}
