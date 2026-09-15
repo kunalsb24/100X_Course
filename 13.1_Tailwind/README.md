1cd0baed4ef87bdb83d176cd8855dd2aa79c3e6b
kunalsb24 <kunalbhajbhuje5@gmail.com>
2026-09-15 11:03:49 +0200

    Added tailwind

--- 13.1_Tailwind/README.md
+++ 13.1_Tailwind/README.md
@@ -5,3 +5,10 @@
 3. Responsiveness
 4. Background color, text color, hover
 
+
+# 13.2 Things to learn
+1. Transitions
+2. Creating Sidebars
+3. Absolute/fixed/relative positioning
+4. Completing the design
+5. Dark Mode, Hover effects
--- 13.1_Tailwind/src/App.jsx
+++ 13.1_Tailwind/src/App.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react"
 import { Otp } from "./components/OTP"
+import { Sidebar } from "./components/Sidebar"
 function App() {
+
  return (
   <>
-    <Otp />
+  
+    {/* <Sidebar /> */}
+    {/* <Otp /> */}
   </>
  
  ) 
